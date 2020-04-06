21a440c5215efff608de92c1823134f19c750518
Randy Lee <rlee@mphi.org>
2020-04-05 23:52:29 -0400

    added migration

---
+++ Migrations/202004060351597_new grio.cs
@@ -0,0 +1,16 @@
+namespace TimeCard.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newgrio : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
